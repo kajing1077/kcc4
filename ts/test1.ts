db030a329d2daaa76dea4919a521c3bc311618fc
kajing1077 <kajing1077@gmail.com>
2024-11-11 13:58:44 +0900

    promise prac

--- ts/test1.ts
+++ ts/test1.ts
@@ -1,23 +1,17 @@
-type Settled<T> =
-  | {
-      status: "fulfilled";
-      value: T;
-    }
-  | {
-      status: "rejected";
-      reason: unknown;
-    };
+export const randTime = <T>(val: T): Promise<T> => {
+  return new Promise((resolve, _) => {
+    setTimeout(resolve, Math.random() * 1000, val);
+  });
+};
 
-export const randTime = <T>(val: T): Promise<T> =>
-  new Promise((resolve) => setTimeout(resolve, Math.random() * 1000, val));
-
-export const promiseAll = <T>(promises: (T | Promise<T>)[]) =>
-  new Promise((resolve, reject) => {
+export const promiseAll = <T>(promises: (T | Promise<T>)[]) => {
+  return new Promise((resolve, reject) => {
     if (!promises?.length) reject(new Error("Promise를 전달하세요!"));
 
     const results: T[] = [];
     let cntToRun = promises.length;
-    for (let i = 0; i < promises.length; i += 1) {
+
+    for (let i = 0; i < promises.length; i++) {
       const promise = (
         promises[i] instanceof Promise
           ? promises[i]
@@ -26,24 +20,43 @@ export const promiseAll = <T>(promises: (T | Promise<T>)[]) =>
       promise
         .then((succ) => {
           results[i] = succ;
-          if ((cntToRun -= 1) === 0) resolve(results);
+          if (--cntToRun === 0) resolve(results);
         })
-        .catch((error) => {
-          reject(error);
-        });
+        .catch((error) => reject(error));
     }
   });
+};
 
-export function promiseAllSettled<T>(promises: Promise<T>[]) {
-  // const results: Promise<Settled<T>>[] = [];
-  // return Promise.all(
+// 객체 전달할때 괄호 안하면 함수로 해석됨
+// 화살표 함수에서 {}로 감쌀때 return 확인할 것
+export const promiseAllSettled = <T>(promises: Promise<T>[]) => {
   return promiseAll(
-    promises.map((promise) =>
-      promise
-        .then((value) => ({ status: "fulfilled", value }))
-        .catch((reason) => ({ status: "rejected", reason }))
-    )
+    promises.map((promise) => {
+      return promise
+        .then((value) => ({
+          status: "fulfilled",
+          value,
+        }))
+        .catch((reason) => ({
+          status: "rejected",
+          reason,
+        }));
+    })
   );
+};
 
-  // return results;
-}
+// export const promiseAllSettled = <T>(promises: Promise<T>[]) => {
+//   return promiseAll(
+//     promises.map((promise) =>
+//       promise
+//         .then((value) => ({
+//           status: "fulfilled",
+//           value,
+//         }))
+//         .catch((reason) => ({
+//           status: "rejected",
+//           reason,
+//         }))
+//     )
+//   );
+// };
